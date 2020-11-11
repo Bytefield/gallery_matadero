cb7b89530ffa23f7559889db408cd5e259c73032
Bytefield <ramosperez1978@gmail.com>
2020-11-11 13:58:35 +0100

    home working, gallery: new images, working on texts

--- gallery.js
+++ gallery.js
@@ -1,12 +1,153 @@
-const images_array = ["foto1.jpg","foto2.jpg","foto3.jpg","foto4.jpg","foto5.jpg","foto6.jpg","foto1.jpg","foto2.jpg","foto3.jpg","foto4.jpg","foto5.jpg","foto6.jpg","foto1.jpg","foto2.jpg","foto3.jpg","foto4.jpg","foto5.jpg","foto6.jpg","foto1.jpg","foto2.jpg","foto3.jpg","foto4.jpg","foto5.jpg","foto6.jpg"];
-const images_path = "./assets/images/gallery/";
+const images = [
+    {
+        name: "digital_mariachi.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "digital_rediseño.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "espacios_mimm.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "espacios_mo.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "grafico_identidadvisual.png",
+        text: "some text here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "grafico_iñor.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "grafico_señaletica.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "grafico_yasuni.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "integral_concentrico.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "integral_manufactura.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "integral_sonidostextiles.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "integral_usule.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "moda_arekuna.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "moda_coleccion.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "moda_escapular.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "moda_puna.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "producto_aplicador.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "producto_frontera.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "producto_kit.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    },
+    {
+        name: "producto_natura.png",
+        text: "some text here",
+        title: "some title here",
+        path: "./assets/images/gallery/categorias/desktop/",
+        tags: []
+    }
+];
+const images_path = "./assets/images/gallery/categorias/desktop/";
 var image_elements_array = [];
 
-const coeficient = 75;
-const number_of_images = images_array.length;
-const scrolling_coeficient = 500 ;
-const scrolling_height = scrolling_coeficient * number_of_images;
+const images_to_display = 7
+const positioning_coeficient = 50;
+const scrolling_coeficient = 1000 ;
 const scrolling_ratio = 2.4;
+const number_total_images = images.length;
+const scrolling_height = scrolling_coeficient * number_total_images;
 const scrolling_ratio_coeficient = scrolling_coeficient / scrolling_ratio;
 
 const gallery_element = document.querySelector('.gallery');
@@ -15,55 +156,127 @@ const scrolling_element = gallery_element.querySelector('.scroll_item ')
 // Set scroll item height for scrolling
 scrolling_element.style.height = scrolling_height + 'px'
 
-images_array.map(function(image, index) {
+images.map(function(image, index) {
 
-    let image_path = images_path + image;
-    let image_class = 'image' + index
+    let image_path = image.path + image.name;
+    let image_class = 'image' + index;
+    let image_text = image.text;
+    let image_title = image.title;
 
-    new_image = document.createElement('img');
+    let new_title = document.createElement('p');
+    new_title.innerText = image_title;
+    new_title.classList.add('image_title');
+    let new_text = document.createElement('p');
+    new_text.innerText = image_text;
+    new_text.classList.add('image_text')
+    let new_image = document.createElement('img');
     new_image.setAttribute('src', image_path);
     new_image.setAttribute('alt', 'image number ' + index);
     new_image.classList.add("image", image_class);
+    let image_container = document.createElement('div');
+    image_container.classList.add('image_container')
+    image_container.append(new_image);
+    image_container.append(new_title);
+    image_container.append(new_text);
 
-    if (index < 7) new_image.classList.add('visible');
 
-    gallery_element.append(new_image);
+    if (index < images_to_display) new_image.classList.add('visible');
+
+    gallery_element.append(image_container);
     image_elements_array.push(new_image);
 
 }, false);
 
-image_elements_array.map(function(element, index) {
+// function to position visible elements
+var position_elements = function(elements_array) {
+    elements_array.map(function(element, index) {
 
-    let move_coeficient = coeficient * index
-    let move_x = move_coeficient;
-    let move_y = move_coeficient * -1;
-    let move_z = move_coeficient * -1;
+        let move_coeficient = positioning_coeficient * index
+        let move_x = move_coeficient * 2.5;
+        let move_y = move_coeficient * -1;
+        let move_z = move_coeficient * -1;
 
-    gsap.set(element, {
-        x: move_x,
-        y: move_y,
-        z: move_z
+        gsap.to(element, {
+            x: move_x,
+            y: move_y,
+            z: move_z
+        });
+
+        element.setAttribute('data-x', move_x);
+        element.setAttribute('data-y', move_y);
+        element.setAttribute('data-z', move_z);
     });
+}
 
-    element.setAttribute('data-x', move_x);
-    element.setAttribute('data-y', move_y);
-    element.setAttribute('data-z', move_z);
-});
-
-
+// initial positioning
+position_elements(image_elements_array);
 
 let counter = 0;
 var scroll_value = 0;
+var last_scroll = 0;
+var forward = true;
+let time_line = new TimelineMax();
 gallery_element.addEventListener('scroll', () => {
+
     scroll_value = gallery_element.scrollTop - (counter * scrolling_ratio_coeficient);
-    if(scroll_value >= scrolling_coeficient) {
+    // let forward = last_scroll < gallery_element.scrollTop;
+
+    if(last_scroll > gallery_element.scrollTop) forward = false;
+    else forward = true;
+
+    let first_in_visible = gallery_element.querySelectorAll('.visible')[0];
+    let last_in_visible = gallery_element.querySelectorAll('.visible')[5]
+    let last_in_visible_index = image_elements_array.indexOf(last_in_visible);
+    let item_one_plus = (last_in_visible_index < image_elements_array.length) ? image_elements_array[last_in_visible_index + 1] : image_elements_array[image_elements_array.length - 1];
+    let first_in_visible_index = image_elements_array.indexOf(first_in_visible);
+    let item_one_minus = (first_in_visible_index > 0) ? image_elements_array[first_in_visible_index - 1] : image_elements_array[0];
+
+    if((scroll_value >= scrolling_coeficient) && forward && (counter <= image_elements_array.length)) {
+
         counter++;
         console.log('click', counter, scroll_value)
-        // first  image?
 
-        // last image?
-        // move images
+        time_line.to(first_in_visible, 0.75, {
+            // rotateY: 45,
+            autoAlpha: 0,
+            onComplete: function() {
+                item_one_plus.classList.add('visible');
+                first_in_visible.classList.remove('visible');
+
+                // reposition all images
+                let visibles_array = [...gallery_element.querySelectorAll('.visible')];
+                position_elements(visibles_array);
+            }
+        });
+    } else if ((scroll_value <= scrolling_coeficient) && !forward && (counter >= 0)){
+
+        counter--;
+        console.log('click', counter, scroll_value)
+
+        time_line.to(last_in_visible, 0.75, {
+            rotateY: 0,
+            autoAlpha: 1,
+            onComplete: function() {
+                time_line.to(item_one_minus, 0.25, {
+                    rotateY: 0,
+                    autoAlpha: 1,
+                    onComplete: function() {
+                        item_one_minus.classList.add('visible');
+                        last_in_visible.classList.remove('visible');
+
+                        // reposition all images
+                        let visibles_array = [...gallery_element.querySelectorAll('.visible')];
+                        position_elements(visibles_array);
+                    }
+                });
+            }
+        });
     }
+
+        last_scroll = gallery_element.scrollTop;
+
+
+        // move images
 });
 
 // identify first and 7+1 in row
--- home.js
+++ home.js
@@ -1,70 +1,165 @@
 // Globals
-var coeficient_rotation_y = 15;
-var coeficient_rotation_x = -5;
+const coeficient_rotation_y = 30;
 
-var left_group = document.querySelector('.left');
-var center_group = document.querySelector('.center');
-var right_group = document.querySelector('.right');
-var groups_array = [...document.querySelectorAll('.group')];
+const scene = document.querySelector('.scene');
+const groups_array = [...document.querySelectorAll('.group')];
+const left_group = document.querySelector('.left');
+const center_group = document.querySelector('.center');
+const image_container = [...document.querySelectorAll('.image_container')];
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const images_elements_array = [...document.querySelectorAll('img')];
 
-var window_width = window.innerWidth;
-var window_center_x = window.innerWidth / 2;
-var window_height = window.innerHeight;
-var window_center_y = window.innerHeight / 2;
+
+const window_width = window.innerWidth;
+const window_center_x = window.innerWidth / 2;
+const window_height = window.innerHeight;
+const window_center_y = window.innerHeight / 2;
+
+gsap.to(one, 0.1, {
+    x: -400,
+    z: -900,
+    rotateY: 25
+})
+
+gsap.to(two, 0.1, {
+    x: -200,
+    z: -1200,
+    rotateY: 25
+})
+
+gsap.to(three, 0.1, {
+    x: 0,
+    z: -1500,
+    rotateY: 0
+})
+
+gsap.to(four, 0.1, {
+    x: -100,
+    z: 0,
+    rotateY: 25
+})
+
+gsap.to(five, 0.1, {
+    x: 150,
+    z: 200,
+    rotateY: 0
+})
+
+gsap.to(six, 0.1, {
+    x: 500,
+    z: 0,
+    rotateY: -25
+})
+
+gsap.to(left_group, 0.1, {
+    left: 0
+})
+
+gsap.to(center_group, 0.1, {
+    left: "40%"
+})
 
 // Gallery rotation function
-
-// START ↓ ↓ ↓
 var rotate_gallery = function(event) {
     let x = event.clientX;
     let y = event.clientY;
 
     let rotation_angle_Y = coeficient_rotation_y * (x - window_center_x)/window_width;
-    let rotation_angle_X = coeficient_rotation_x * (y - window_center_y)/window_height;
 
-    gsap.to('.scene', {
-        rotateY: rotation_angle_Y ,
-        rotateX: rotation_angle_X
+    gsap.to(scene, {
+        rotateY: rotation_angle_Y
     })
 }
 
 document.addEventListener('mousemove', rotate_gallery);
-// END ↑ ↑ ↑
+
+// zoom image on click when active group
+var open_image = function(element) {
+
+    let element_parent = element.parentElement;
+    let target_element = element.parentElement;
+    let parent_group = element.closest('.group');
+
+    if(element_parent.classList.contains('clicked')) {
+        element_parent.classList.remove('clicked');
+    } else {
+        element_parent.classList.add('clicked');
+    }
+
+    let element_not_clicked = scene.querySelectorAll('.image_container:not(.clicked)');
+
+    let element_tl = new TimelineMax({ paused: true});
+    let element_not_clicked_tl = new TimelineMax({ paused: true});
+    let target_element_tl = new TimelineMax({ paused: true});
+    let parent_group_tl = new TimelineMax({ paused: true});
+    let scene_tl = new TimelineMax({ paused: true});
+
+    element_tl.to(element, 0.5, {
+        width: "100vw",
+        height: "100vh",
+        top: 0,
+        left:0,
+        rotateY: 0
+    })
+
+    element_not_clicked_tl.to(element_not_clicked, 0.5, {
+        autoAlpha: 0
+    })
+
+    target_element_tl.to(target_element, 1, {
+        rotateY: 0,
+        x: 0,
+        y: 0,
+        z: 0
+    })
+
+    parent_group_tl.to(parent_group, 1, {
+        left: 0
+    })
+
+    scene_tl.to(scene, 1, {
+        top: 0,
+        rotateY: 0,
+        x: 0,
+        z: 0
+    })
+
+    element_tl.play();
+    element_not_clicked_tl.play();
+    target_element_tl.play();
+    parent_group_tl.play();
+    scene_tl.play();
+    document.removeEventListener('mousemove', rotate_gallery)
+}
 
 // Detect position of clicked element and reposition
-
-// START ↓ ↓ ↓
 var detect_component = (function() {
 
-    let image_array = [...document.querySelectorAll('.image')];
+    let image_array = [...document.querySelectorAll('.group img')];
 
     image_array.map((element)=>{
         element.addEventListener('click', (event)=> {
+
             let clicked_element = event.target;
             let clicked_group = clicked_element.closest('.group');
-            // group has class active? stay : add class active
-            if (!clicked_group.classList.contains('active')) {
+
+            if (clicked_group.classList.contains('active')) {
+                open_image(clicked_element);
+            } else {
                 document.querySelector('.active').classList.remove('active');
                 clicked_group.classList.add('active');
-            } else {
                 reposition(clicked_group);
             }
         })
-        element.addEventListener('mouseenter', function(event) {
-            let image = event.target;
-            image.classList.add('hovered');
-        });
-        element.addEventListener('mouseleave', function(event) {
-            let image = event.target;
-            image.classList.remove('hovered');
-        });
     })
 })()
-// END ↑ ↑ ↑
 
 // Reposition camera on click
-
-// START ↓ ↓ ↓
 var reposition = function(element) {
 
     let modifier_x = 0;
@@ -76,22 +171,95 @@ var reposition = function(element) {
         modifier_x = 1;
         modifier_z = 1;
         modifier_rotation = -1;
-    } else if (element.classList.contains('right')) {
-        modifier_x = -1;
-        modifier_z = 1;
-        modifier_rotation = 0;
+
     } else {
         modifier_x = 0;
         modifier_z = 0;
         modifier_rotation = 1;
     }
 
-    time_line  .to('.scene', {
-            x: 450 * modifier_x
-        })
-        .to('.scene', {
-            z: 800 * modifier_z,
+    time_line
+        .to('.scene', 1, {
+            ease: Sine.easeOut,
+                x: 250 * modifier_x
+            })
+        .to('.scene', 1, {
+            ease: Sine.easeOut,
+            z: 2000 * modifier_z,
             rotateY: 20 * modifier_rotation
         })
 }
-// END ↑ ↑ ↑
+
+const close_button = [...document.querySelectorAll('.close_button')];
+close_button.map(function(element) {
+    element.addEventListener('click', function() {
+        position_elements();
+    })
+});
+
+// Getting all elements in their original position
+const position_elements = function() {
+
+    gsap.to(one, 0.1, {
+        x: -400,
+        z: -900,
+        rotateY: 25
+    })
+
+    gsap.to(two, 0.1, {
+        x: -200,
+        z: -1200,
+        rotateY: 25
+    })
+
+    gsap.to(three, 0.1, {
+        x: 0,
+        z: -1500,
+        rotateY: 0
+    })
+
+    gsap.to(four, 0.1, {
+        x: -100,
+        z: 0,
+        rotateY: 25
+    })
+
+    gsap.to(five, 0.1, {
+        x: 150,
+        z: 200,
+        rotateY: 0
+    })
+
+    gsap.to(six, 0.1, {
+        x: 500,
+        z: 0,
+        rotateY: -25
+    })
+
+    gsap.to(left_group, 0.1, {
+        left: 0
+    });
+
+    gsap.to(center_group, 0.1, {
+        left: "40%"
+    });
+
+    gsap.to(images_elements_array, 0.1, {
+        width: 350,
+        height: "auto"
+    });
+
+    gsap.to(image_container, 0.1, {
+        autoAlpha: 1
+    });
+
+    image_container.map(function(element) {
+        if (element.classList.contains('clicked')) element.classList.remove('clicked');
+    });
+
+    gsap.to(scene, 0.1, {
+        top: "calc(50% - 125px)"
+    });
+
+    document.addEventListener('mousemove', rotate_gallery);
+}
