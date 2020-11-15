9a8d5a99d7026c4b6a164c34542c6b786027b344
Bytefield <ramosperez1978@gmail.com>
2020-11-15 09:01:20 +0100

    1st round changes with nicola

--- programa.js
+++ programa.js
@@ -7,24 +7,140 @@ const window_center_y = window.innerHeight / 2;
 const coeficient_rotation_y = 15;
 const content = document.querySelector('.content')
 
+const left = content.querySelector('.left');
+const left_top = left.querySelector('.top');
+const first = left_top.querySelector('img:first-of-type');
+const second = left_top.querySelector('img:last-of-type');
+const left_bottom = left.querySelector('.bottom');
+const third = left_bottom.querySelector('img');
+const right = content.querySelector('.right');
+const fourth = right.querySelector('.right img:first-of-type')
+const fifth = right.querySelector('.right img:last-of-type')
+
+gsap.set(left, {
+    x: 190,
+    y: 75,
+    z: -10,
+    rotateY: 52
+})
+
+gsap.set(first, {
+    x: 35,
+    y: 10,
+    rotateY: 15
+})
+
+gsap.set(second, {
+    x: 20,
+    y: 74
+})
+
+gsap.set(third, {
+    x: 194,
+    y: -24,
+    z: 65
+})
+
+gsap.set(right, {
+    x: 135,
+    y: 150,
+    z: -125,
+    rotateY: -60
+})
+
+gsap.set(fourth, {
+    x: 120,
+    y: 20,
+    z: -30,
+    rotateY: -10
+})
+
+console.log(fourth);
+
+gsap.set(fifth, {
+    x: -120,
+    y: -25,
+    z: 25
+})
+
+const reset_transform = function() {
+    gsap.to(left, {
+        x: 0,
+        y: 0,
+        z: 0,
+        rotateY: 0
+    })
+
+    gsap.to(second, {
+        x: 0,
+        y: 0,
+        z: 0,
+    })
+
+    gsap.to(third, {
+        x: 0,
+        y: 0,
+        z: 0,
+    })
+
+    gsap.to(right, {
+        x: 0,
+        y: 0,
+        z: 0,
+        rotateY: 0
+    })
+
+    gsap.to(fourth, {
+        x: 0,
+        y: 0,
+        z: 0,
+    })
+
+    gsap.to(fifth, {
+        x: 0,
+        y: 0,
+        z: 0,
+    })
+}
+
+var toggleClass = function(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
+
 const images_array = [...document.querySelectorAll('img')];
+var element_prev_width = 0;
 
 images_array.map(function(element) {
 
-    element.addEventListener('click', function() {
-        gsap.to(element, {
-            width: "60vw"
+    element.addEventListener('click', function(event) {
+        reset_transform();
+        toggleClass(element, 'active');
+
+        gsap.to(event.target, {
+            position: "absolute",
+            width: "70vw",
+            left: "15vw",
+            top: "10vh"
         })
+
+        gsap.to(event.target.parentElement, {
+            position: "absolute",
+            left: 0,
+            top: 0
+        })
+        let elements_not_active = [...document.querySelectorAll('img:not(.active)')];
+        elements_not_active.map(function(element) {
+            gsap.to(element, 0.1, {
+                autoAlpha: 0
+            })
+        });
     })
 
-    let element_parent = element.parentElement;
-    let element_parent_2nd = element_parent.parentElement;
-    let element_parent_3rd = element_parent_2nd.parentElement;
 
-    var element_css = "";
-    var element_parent_css = "";
-    var element_parent_2nd_css = "";
-    var element_parent_3rd_css = "";
 
     // On mouse enter/leave not working as it should
 
